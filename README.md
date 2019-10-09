27c5ac5a91dea3fcfda265a1b219e3d0a2112db7
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-09 18:28:07 +0800

    Deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mqtt-express-node-geneva
 
-https://mqtt-express-node-geneva.herokuapp.com/
-https://git.heroku.com/mqtt-express-node-geneva.git
+https://geneva-mqtt-fan-switch.herokuapp.com/
+https://git.heroku.com/geneva-mqtt-fan-switch.git
--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -15,10 +15,6 @@ $(document).ready(function() {
         let topic = "geneva/fan/status";
         let payload = "Turned off: " + moment().format('MMMM Do YYYY, h:mm:ss a');
         client.publish(topic, payload);
-        $("#btn1").attr("disabled", false)
-        $("#btn2").attr("disabled", false)
-        $("#btn3").attr("disabled", false)
-        $("#btnOff").attr("disabled", true)
         $("#message").text("The fan is currently turned off")
     })
     $("#btn1").click(function(e) {
@@ -26,10 +22,6 @@ $(document).ready(function() {
         let topic = "geneva/fan/status";
         let payload = "Turned at 1: " + moment().format('MMMM Do YYYY, h:mm:ss a');
         client.publish(topic, payload);
-        $("#btnOff").attr("disabled", false)
-        $("#btn2").attr("disabled", false)
-        $("#btn3").attr("disabled", false)
-        $("#btn1").attr("disabled", true)
         $("#message").text("The fan is currently turned at 1")
     })
     $("#btn2").click(function(e) {
@@ -37,10 +29,6 @@ $(document).ready(function() {
         let topic = "geneva/fan/status";
         let payload = "Turned at 2: " + moment().format('MMMM Do YYYY, h:mm:ss a');
         client.publish(topic, payload);
-        $("#btnOff").attr("disabled", false)
-        $("#btn1").attr("disabled", false)
-        $("#btn3").attr("disabled", false)
-        $("#btn2").attr("disabled", true)
         $("#message").text("The fan is currently turned at 2")
 
     })
@@ -49,10 +37,6 @@ $(document).ready(function() {
         let topic = "geneva/fan/status";
         let payload = "Turned at 3: " + moment().format('MMMM Do YYYY, h:mm:ss a');
         client.publish(topic, payload);
-        $("#btnOff").attr("disabled", false)
-        $("#btn1").attr("disabled", false)
-        $("#btn2").attr("disabled", false)
-        $("#btn3").attr("disabled", true)
         $("#message").text("The fan is currently turned at 3")
     })
 })
